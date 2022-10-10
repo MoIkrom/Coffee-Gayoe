9ada0b803faa93ac619dab7229f3a1199aeaa695
MoIkrom <mikram216@gmail.com>
2022-10-10 23:52:15 +0700

    config

--- src/config/postgre.js
+++ src/config/postgre.js
@@ -1,12 +1,12 @@
+require("dotenv").config();
+const { DB_HOST_DEV, DB_USER_DEV, DB_PASS_DEV, DB_NAME_DEV, DB_PORT } = process.env;
 const { Pool } = require("pg");
-// const { Pool } = pg;
-
 const db = new Pool({
-  host: "localhost",
-  user: "postgres",
-  database: "coffe_gayoe",
-  password: "612mikram",
-  port: 5432,
+  host: DB_HOST_DEV,
+  user: DB_USER_DEV,
+  database: DB_NAME_DEV,
+  password: DB_PASS_DEV,
+  port: DB_PORT,
 });
 
 module.exports = db;
