13fa32b5ffbf7838300dc8349eb9bc18f3f342c0
MoIkrom <mikram216@gmail.com>
2022-10-16 20:28:25 +0700

    update Index

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+// import env
+require("dotenv").config();
+// import express
 const express = require("express");
-
 // import db
 const postgreDb = require("./src/config/postgre");
 // import mainRouter
-const mainRouter = require("./src/routes/main");
+const mainRouter = require("./src/routes/rt_main");
+// import morgan
+const morgan = require("morgan");
+
 // init express application
 const server = express();
 const PORT = 8080;
@@ -13,16 +18,19 @@ postgreDb
   .then(() => {
     // tes connect DB
     console.log("DB connected");
-    // parser untuk body
-    server.use(express.json());
-    server.use(express.urlencoded({ extended: false }));
-    // extended true  => parsing menggunakan querystring => bisa memproses nested object
-    // extended false => parsing menggunakan querystring => tidak bisa memproses nested object
 
-    // semua request ke server akan dikirim ke mainRouter
+    // init morgan
+    server.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
+
+    // init parser body
+    server.use(express.json());
+
+    server.use(express.urlencoded({ extended: false }));
+
+    // mainRouter
     server.use(mainRouter);
 
-    // server siap menerima request di port
+    // run server di port
     server.listen(PORT, () => {
       console.log(`Server is running at port ${PORT}`);
     });
