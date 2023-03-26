fb0156b5cbe277b410b85b1748a8cc2195d247c6
MoIkrom <mikram216@gmail.com>
2023-03-26 22:10:00 +0700

    nodemailer

---
+++ src/config/gmail.js
@@ -0,0 +1,20 @@
+const { google } = require("googleapis");
+
+const clientId = process.env.CLIENT_ID_GOOGLE;
+const clientsecret = process.env.CLIENT_SECRET_GOOGLE;
+const refreshToken = process.env.REFRESH_TOKEN_GOOGLE;
+
+const { OAuth2 } = gooogle.auth;
+const OAuth2Client = new OAuth2(clientId, clientsecret);
+OAuth2Client.setCredentials({
+  refresh_token: refreshToken,
+});
+
+const accessToken = OAuth2Client.getAccessToken;
+
+module.exports = {
+  clientId,
+  clientsecret,
+  refreshToken,
+  accessToken,
+};
