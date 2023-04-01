155991d2ebcdc3b5e90632f86e4388934b151af6
MoIkrom <mikram216@gmail.com>
2023-04-01 14:29:59 +0700

    register with username

--- src/routes/rt_users.js
+++ src/routes/rt_users.js
@@ -46,45 +46,9 @@ usersRouter.get("/profile", isLogin(), allowedRole("user"), getProfile);
 // ======================
 // Punya Acil
 usersRouter.patch("/profile", isLogin(), allowedRole("user"), uploadFiles, cloudinary, profile);
-// ======================
-usersRouter.post("/", validate.body("email", "password", "phone_number", "firstname", "lastname"), create);
+usersRouter.post("/", validate.body("email", "password", "username"), create);
 usersRouter.patch("/editpassword", isLogin(), allowedRole("user", "admin"), editPassword);
 usersRouter.patch("/", isLogin(), allowedRole("user"), uploadFile, cloudinary, edit);
 usersRouter.delete("/:id", isLogin(), allowedRole("admin"), drop);
 
 module.exports = usersRouter;
-
-// =========================================================
-
-// const express = require("express");
-// const usersRouter = express.Router();
-// // const checkDuplicate = require("../middlewares/M_authentication");
-// const { checkDuplicateEmail, checkDuplicatePhoneNumber } = require("../middlewares/M_authentication");
-// const { register, editPassword, createNewProfile, get, getById } = require("../controllers/C_users");
-// // const { checkToken } = require("../middlewares/isLogin");
-// const isLogin = require("../middlewares/isLogin");
-// const uploadImage = require("../middlewares/M_upload");
-// // const upload = uploadImage.single("image");
-// const allowedRole = require("../middlewares/M_allowedRole");
-
-// // //register
-// // usersRouter.post("/", checkDuplicate, register);
-// usersRouter.post("/", checkDuplicateEmail, checkDuplicatePhoneNumber, register);
-// // // Router.post("/new", , authController.register);
-
-// // Edit Password
-// usersRouter.patch("/account", editPassword);
-
-// // usersRouter.get("/", checkToken, get);
-// // usersRouter.get("/", get);
-// usersRouter.get("/", isLogin(), allowedRole("Admin"), get);
-// usersRouter.get("/", isLogin(), allowedRole("user"), getById);
-// // usersRouter.get("/", checkToken, allowedRole("Admin"), get);
-// usersRouter.post("/profile", createNewProfile);
-// // usersRouter.post("/profile", checkToken, upload, createNewProfile);
-// // edit Profile
-// // usersRouter.patch("/profile/edit", checkToken, editPassword);
-// // usersRouter.delete("/:id", drop);
-// // usersRouter.patch("/edit/password", password);
-
-// module.exports = usersRouter;
