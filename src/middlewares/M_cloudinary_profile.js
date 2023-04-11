b724dae2c2f66dcb74191e5df7018fa06131c754
MoIkrom <mikram216@gmail.com>
2023-04-12 00:41:36 +0700

    cloudinary

--- src/middlewares/M_cloudinary_profile.js
+++ src/middlewares/M_cloudinary_profile.js
@@ -2,30 +2,30 @@ const cloudinary = require("../config/cloudinary");
 const DatauriParser = require("datauri/parser");
 const path = require("path");
 
-const uploaderProfile = async (req, res, next) => {
-  const { body, file } = req;
-  if (!file) return next();
+module.exports = {
+  uploaderProfile: async (req, res, next) => {
+    const { body, file } = req;
+    if (!file) return next();
 
-  const parser = new DatauriParser();
-  const buffer = file.buffer;
-  const ext = path.extname(file.originalname).toString();
-  const datauri = parser.format(ext, buffer);
-  const filename = `${body.username}`;
-  const cloudinaryOpt = {
-    // use_filename: true,
-    public_id: filename,
-    overwrite: true,
-    folder: "coffee-gayoe",
-  };
+    const parser = new DatauriParser();
+    const buffer = file.buffer;
+    const ext = path.extname(file.originalname).toString();
+    const datauri = parser.format(ext, buffer);
+    const filename = `${body.username}`;
+    const cloudinaryOpt = {
+      // use_filename: true,
+      public_id: filename,
+      overwrite: true,
+      folder: "coffee-gayoe",
+    };
 
-  try {
-    const result = await cloudinary.uploader.upload(datauri.content, cloudinaryOpt);
-    req.file = result;
-    next();
-  } catch (err) {
-    console.log(err.message);
-    res.status(err).json({ msg: "Internal Server Error" });
-  }
+    try {
+      const result = await cloudinary.uploader.upload(datauri.content, cloudinaryOpt);
+      req.file = result;
+      next();
+    } catch (err) {
+      console.log(err.message);
+      res.status(err).json({ msg: "Internal Server Error" });
+    }
+  },
 };
-
-module.exports = uploaderProfile;
--- src/routes/rt_users.js
+++ src/routes/rt_users.js
@@ -5,9 +5,8 @@ const allowedRole = require("../middlewares/M_allowedRole");
 const isLogin = require("../middlewares/isLogin");
 const validate = require("../middlewares/M_validate");
 const { memoryUpload, errorHandler } = require("../middlewares/M_upload");
-const cloudinary = require("../middlewares/M_cloudinary_profile");
+const { uploaderProfile } = require("../middlewares/M_cloudinary_profile");
 const { get, create, edit, drop, editPassword, getProfile, profile, verify } = require("../controllers/C_users");
-// const checkDuplicate = require("../middlewares/M_checkDuplicate");
 
 function uploadFile(req, res, next) {
   const upload = memoryUpload.single("image");
@@ -26,30 +25,12 @@ function uploadFile(req, res, next) {
   });
 }
 
-// Punya Acil
-function uploadFiles(req, res, next) {
-  memoryUpload.single("image")(req, res, function (err) {
-    if (err instanceof multer.MulterError) {
-      console.log(err);
-      return res.status(400).json({ msg: err.message });
-    } else if (err) {
-      return res.json({ msg: err.message });
-    }
-    next();
-  });
-}
-// ========================
-
 usersRouter.get("/", isLogin(), allowedRole("admin"), get);
 usersRouter.get("/profile", isLogin(), allowedRole("user"), getProfile);
-
-// ======================
-// Punya Acil
-usersRouter.patch("/profile", isLogin(), allowedRole("user"), uploadFiles, cloudinary, profile);
 usersRouter.post("/", validate.body("email", "password", "username"), create);
 usersRouter.post("/verify/:id", verify);
 usersRouter.patch("/editpassword", isLogin(), allowedRole("user", "admin"), editPassword);
-usersRouter.patch("/", isLogin(), allowedRole("user"), uploadFile, cloudinary, edit);
+usersRouter.patch("/", isLogin(), allowedRole("user"), uploadFile, uploaderProfile, edit);
 usersRouter.delete("/:id", isLogin(), allowedRole("admin"), drop);
 
 module.exports = usersRouter;
